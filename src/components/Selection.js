2f450e4358929423463941e16e91ddabf4fc8f10
[Sujal Wadhankar] <[sujalwadhankar938@gmail.com]>
2026-09-02 18:12:48 +0530

    Implement color selection with handler prop

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
+import React, { useState } from "react";
 
+const Selection = ({ applyColor }) => {
+  const [style, setStyle] = useState({ background: "" });
+
+  return (
+    <div
+      className="fix-box"
+      style={style}
+      onClick={() => applyColor(setStyle)}
+    >
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+};
+
+export default Selection;
